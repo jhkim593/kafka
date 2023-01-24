3710db87dacc5faf775a60b80cabed4de0af0e5f
jhkim593 <fpdlwjzlr@naver.com>
2023-01-24 23:38:34 +0900

    -

---
+++ producers/src/main/java/com/example/kafka/SimpleProducer.java
@@ -0,0 +1,32 @@
+package com.example.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.util.Properties;
+
+public class SimpleProducer {
+
+    public static void main(String[] args) {
+
+        String topicName = "simple-topic";
+
+        Properties props = new Properties();
+
+        props.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG,"192.168.56.101:9092");
+        props.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        props.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> kafkaProducer = new KafkaProducer<String, String>(props);
+
+        ProducerRecord<String, String> producerRecord = new ProducerRecord<>(topicName, "helloWorld");
+
+        kafkaProducer.send(producerRecord);
+
+        kafkaProducer.flush();
+
+
+    }
+}
